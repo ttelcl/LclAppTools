ebe85860d9e43b02dc22c8b6e1ea452cc47cad4b
Luc Cluitmans <9429819+ttelcl@users.noreply.github.com>
2021-12-15 22:49:56 +0200

    Created a stub for a new library for file system utility support

---
+++ Lcl.FilesystemUtilities/Modeling/FsTreeNode.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * (c) 2021  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lcl.FilesystemUtilities.Modeling
+{
+  /// <summary>
+  /// A node in a file system tree (a directory, file or symbolic link)
+  /// </summary>
+  public class FsTreeNode
+  {
+    /// <summary>
+    /// Create a new FsTreeNode
+    /// </summary>
+    public FsTreeNode()
+    {
+      throw new NotImplementedException();
+    }
+
+    public static FsTreeNode FromFileSystemInfo(FileSystemInfo fsi)
+    {
+      if(fsi.Attributes.HasFlag(FileAttributes.ReparsePoint))
+      {
+        // TBD
+      }
+      throw new NotImplementedException();
+    }
+
+  }
+}
---
+++ Lcl.FilesystemUtilities/_readme_.md
@@ -0,0 +1,8 @@
+﻿### Lcl.FilesystemUtilities
+
+Utilities to aid in building file system apps. Requires
+.NET 6, because it uses some APIs only available there,
+such as the Symbolic Link support functions.
+
+
+
