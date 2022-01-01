f1c6a194940b0bbf88d1ad6904d9f948c0f6720a
Luc Cluitmans <9429819+ttelcl@users.noreply.github.com>
2022-01-01 22:51:06 +0200

    Lcl.FileSystemUtilities: started redesign of filesystem model classes

---
+++ Lcl.FilesystemUtilities/FsModel/DirectoryNode.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * (c) 2022  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lcl.FilesystemUtilities.FsModel
+{
+  /// <summary>
+  /// A concrete ContainerNode that is not a root and has a name
+  /// </summary>
+  public class DirectoryNode: ContainerNode
+  {
+    /// <summary>
+    /// Create a new DirectoryNode (normally invoked via ContainerNode.AddDirectory())
+    /// </summary>
+    /// <param name="parent">
+    /// The parent node for this directory node (not nullable!)
+    /// </param>
+    /// <param name="name">
+    /// The name of the directory (not nullable)
+    /// </param>
+    internal DirectoryNode(ContainerNode parent, string name)
+      : base(parent, name)
+    {
+    }
+
+  }
+}
