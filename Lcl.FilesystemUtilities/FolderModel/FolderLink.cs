0075c1581f5adcf97f132465501a754ce6e51235
Luc Cluitmans <9429819+ttelcl@users.noreply.github.com>
2022-03-22 22:57:54 +0200

    Another draft for modeling directory trees (FolderModel)

---
+++ Lcl.FilesystemUtilities/FolderModel/FolderLink.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * (c) 2022  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lcl.FilesystemUtilities.FolderModel
+{
+  /// <summary>
+  /// A FolderItem that is a link to another folder (identified
+  /// by target name)
+  /// </summary>
+  public class FolderLink: FolderItem
+  {
+    /// <summary>
+    /// Create a new FolderLink that resides in a FolderContainer
+    /// </summary>
+    internal FolderLink(FolderContainer parent, string name, string target)
+      : base(parent.Host, parent, name)
+    {
+      Target = target;
+    }
+
+    /// <summary>
+    /// Create a root FolderLink (for example to represent a SUBST drive). Nameless
+    /// </summary>
+    internal FolderLink(FolderSystem host, string target)
+      : base(host, null, "")
+    {
+      Target = target;
+    }
+
+    /// <summary>
+    /// The name of the target the link points to
+    /// </summary>
+    public string Target { get; }
+
+  }
+}
---
+++ Lcl.FilesystemUtilities/FolderModel/FolderSystem.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * (c) 2022  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lcl.FilesystemUtilities.FolderModel
+{
+  /// <summary>
+  /// A group of related folder trees, sharing naming rules, and allowing links
+  /// between trees.
+  /// </summary>
+  public class FolderSystem
+  {
+    private readonly Dictionary<string, FolderItem> _anchors;
+
+    /// <summary>
+    /// Create a new FolderSystem
+    /// </summary>
+    public FolderSystem(bool caseSensitive)
+    {
+      CaseSensitive = caseSensitive;
+      NameComparer = CaseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase;
+      _anchors = new Dictionary<string, FolderItem>(NameComparer);
+      Anchors = _anchors;
+    }
+
+    /// <summary>
+    /// Whether or not name comparisons are case sensitive
+    /// </summary>
+    public bool CaseSensitive { get; }
+
+    /// <summary>
+    /// The name comparer (as determined by CaseSensitive)
+    /// </summary>
+    public StringComparer NameComparer { get; }
+
+    /// <summary>
+    /// The anchors: the root folders and / or links acting as roots
+    /// </summary>
+    public IReadOnlyDictionary<string, FolderItem> Anchors { get; }
+
+    /// <summary>
+    /// Add a new anchor folder.
+    /// Anchor folders are nameless, but have a label that identifies them.
+    /// </summary>
+    /// <param name="label">
+    /// The label for the anchor
+    /// </param>
+    /// <returns>
+    /// The new (nameless) anchor folder
+    /// </returns>
+    public FolderContainer AddAnchorFolder(string label)
+    {
+      if(_anchors.ContainsKey(label))
+      {
+        throw new InvalidOperationException(
+          $"Duplicate anchor label: {label}");
+      }
+      var anchor = new FolderContainer(this);
+      _anchors[label] = anchor;
+      return anchor;
+    }
+
+    /// <summary>
+    /// Add a new anchor link. 
+    /// Anchor links are nameless, but have a label that identifies them.
+    /// </summary>
+    /// <param name="label">
+    /// The label for the anchor
+    /// </param>
+    /// <param name="target">
+    /// The target for the link
+    /// </param>
+    /// <returns>
+    /// The new anchor link
+    /// </returns>
+    public FolderLink AddAnchorLink(string label, string target)
+    {
+      if(_anchors.ContainsKey(label))
+      {
+        throw new InvalidOperationException(
+          $"Duplicate anchor label: {label}");
+      }
+      var anchor = new FolderLink(this, target);
+      _anchors[label] = anchor;
+      return anchor;
+    }
+
+  }
+}
